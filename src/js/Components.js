2a719834af75f2e0536a5073679b5fc02e3bd40b
TMTY <salaudeen.t@gmail.com>
2021-04-21 02:01:00 +0100

    page scroll

--- src/js/Components.js
+++ src/js/Components.js
@@ -7,17 +7,17 @@ export class Components {
         return cards.reduce((acc, curr, index) => {
             return acc + `
                 <div class="card song" data-src="./Music/Brooklyn-Baby.m4a" data-position="" >
-                        <img src="${curr.imgSource}" alt="song_image">
-                        <div class="song_details-action">
-                            <button class="play"><i class="material-icons play">play_arrow</i></button>
-                            <div class="song_details">
-                                <p>${curr.title}</p>
-                                <p>${curr.artistes.reduce((acc, artiste, i) => {
+                        <img class="card-img" src="${curr.imgSource}" alt="song_image">
+                        <div class="card-details">
+                            <button class="play card-play"><i class="material-icons play">play_arrow</i></button>
+                            <div class="card-title-artiste">
+                                <p class="card-title">${curr.title}</p>
+                                <p class="card-artiste">${curr.artistes.reduce((acc, artiste, i) => {
                                     return acc + `<span class="artiste">${artiste}${i === curr.artistes.length - 1 ? '' : ', '}</span>`
                                 }, ``)}
                                 </p>
                             </div>
-                            <button class="add"><i class="material-icons add" >add</i></button>
+                            <button class="add card-add"><i class="material-icons add" >add</i></button>
                         </div>
                     </div>`
         }, ``)
@@ -103,11 +103,11 @@ export class Components {
 
     static navigation = (links) => {
 
-        return `<nav id="link">
+        return `<nav class="page-links">
                     <ul>
                         ${links.reduce((acc, curr, i) => {
                             return acc + `
-                                <li class='link ${i === 0 ? 'active-link' : ''}'><a href="#${curr}">${curr} <hr></a></li>`
+                                <li class='page-link ${i === 0 ? 'active-link' : ''}'><a href="#${curr}">${curr} <hr></a></li>`
                         }, ``)}
                     </ul>
                 </nav>`
@@ -118,7 +118,7 @@ export class Components {
         <div class="cards">
             ${cards.reduce((acc, curr) => {
                 return acc + `
-                    <div class="card" id="genre">
+                    <div class="card genre-card">
                         <img src="${curr.imgSrc}" alt="image">
                         <div class="genre-overlay">
                             <p>${curr.genre}</p>
@@ -132,8 +132,8 @@ export class Components {
 
         return sections.reduce((acc, curr) => {
             return acc + `
-                <section>
-                    <header>${this.header(curr.header)}</header>
+                <section class="page-section">
+                    <header class="page-section-header">${this.header(curr.header)}</header>
                     <div class="cards">${id === "genre" ? this.genreCard(curr.cards) : this.card(curr.cards)}</cards>
                 </section>`
         }, ``)
