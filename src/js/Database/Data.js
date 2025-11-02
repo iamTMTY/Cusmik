df0f1308dad4d3da659cb7815647404abe98e78b
iamTMTY <salaudeen.t@gmail.com>
2025-11-02 08:40:32 +0100

    updated songs database

--- src/js/Database/Data.js
+++ src/js/Database/Data.js
@@ -1,80 +1,57 @@
 export const songs = [
 	{
 		id: "1",
-		title: "Brooklyn Baby",
-		artistes: ["Lana Del Rey"],
-		imgSource: "./imgs/lana.jpg",
+		title: "Re:バース (Re:Verse)",
+		artistes: ["439*hz"],
+		imgSource: "https://linkstorage.linkfire.com/medialinks/images/da45630e-cc50-4a6c-8027-a1697a22efa8/artwork-440x440.jpg",
 		musicSrc:
-			"https://res.cloudinary.com/temitayo/video/upload/v1630710596/cusmik/Brooklyn-Baby_woyefm.m4a"
+			"https://ncs.io/track/download/b5700ce1-ddd4-447d-a55e-840ce62787aa"
 	},
 	{
 		id: "2",
-		title: "Forgotten Love",
-		artistes: ["Aurora"],
-		imgSource: "./imgs/aurora.jpg",
+		title: "Royalty Funk",
+		artistes: ["LXNGVX"],
+		imgSource: "https://linkstorage.linkfire.com/medialinks/images/63e390e7-226e-4ba6-8d5b-61ee8e12dc14/artwork-440x440.jpg",
 		musicSrc:
-			"https://res.cloudinary.com/temitayo/video/upload/v1630710511/cusmik/Forgotten_Love_sgxjgv.mp3"
+			"https://ncs.io/track/download/7a583241-0e27-41ee-909a-7eb878f7fb71"
 	},
 	{
 		id: "3",
-		title: "Fuck It I Love You",
-		artistes: ["Lana Del Rey"],
-		imgSource: "./imgs/lana.jpg",
+		title: "Royalty",
+		artistes: ["Maestro Chives", "Egzod", "Neoni"],
+		imgSource: "https://linkstorage.linkfire.com/medialinks/images/374fc4ba-fe39-4bcf-9cf0-74c87c879ed0/artwork-440x440.jpg",
 		musicSrc:
-			"https://res.cloudinary.com/temitayo/video/upload/v1630710519/cusmik/Fuck_it_I_love_you_iizaws.mp3"
+			"https://ncs.io/track/download/2b08f7d9-4f5f-47bd-a608-07dec1f7eaa9"
 	},
 	{
 		id: "4",
-		title: "Hoax",
-		artistes: ["Taylor Swift"],
-		imgSource: "./imgs/folklore.jpg",
-		musicSrc: "https://res.cloudinary.com/temitayo/video/upload/v1630710566/cusmik/Hoax_mvvvcx.mp3"
+		title: "デイドリーム (Daydream)",
+		artistes: ["RINZO", "MAHIRU"],
+		imgSource: "https://linkstorage.linkfire.com/medialinks/images/d1e5d322-eef0-4c13-a3e5-35aceb3a036d/artwork-440x440.jpg",
+		musicSrc: "https://ncs.io/track/download/8cd5db57-c4b6-4ebe-8090-eca87cbb0342"
 	},
 	{
 		id: "5",
-		title: "Money Power Glory",
-		artistes: ["Lana Del Rey"],
-		imgSource: "./imgs/lana.jpg",
+		title: "Mortals (feat. Laura Brehm)",
+		artistes: ["Warriyo", "Laura Brehm"],
+		imgSource: "https://linkstorage.linkfire.com/medialinks/images/17534773-8bd9-419c-b46d-d500a4af33df/artwork-440x440.jpg",
 		musicSrc:
-			"https://res.cloudinary.com/temitayo/video/upload/v1630710418/cusmik/Money_Power_Glory_lriafg.m4a"
+			"https://ncs.io/track/download/784a2ccc-5ace-48d1-8af1-9da55c383960"
 	},
 	{
 		id: "6",
-		title: "No Pressure",
-		artistes: ["Logic"],
-		imgSource: "./imgs/logic.jpg",
+		title: "Mortals Funk Remix",
+		artistes: ["Warriyo", "LXNGVX"],
+		imgSource: "https://linkstorage.linkfire.com/medialinks/images/af32fef8-3cc4-4fc1-ad89-542b61dc5c32/artwork-440x440.jpg",
 		musicSrc:
-			"https://res.cloudinary.com/temitayo/video/upload/v1630710601/cusmik/No_Pressure_tec85p.mp3"
+			"https://ncs.io/track/download/533bfba3-7868-4ea2-a161-5d862e13b4b0"
 	},
 	{
 		id: "7",
-		title: "Pain 1993",
-		artistes: ["Drake", "Playboi Carti"],
-		imgSource: "./imgs/drake.jpg",
+		title: "Montagem Toma",
+		artistes: ["X972", "sk3tch01", "MXZI"],
+		imgSource: "https://linkstorage.linkfire.com/medialinks/images/282d992a-6c53-41be-8ba0-a1d43057eb41/artwork-440x440.jpg",
 		musicSrc:
-			"https://res.cloudinary.com/temitayo/video/upload/v1630710433/cusmik/Pain_1993_feat._Playboi_Carti_bblu38.mp3"
-	},
-	{
-		id: "8",
-		title: "Snowflake",
-		artistes: ["Sia"],
-		imgSource: "./imgs/aurora.jpg",
-		musicSrc:
-			"https://res.cloudinary.com/temitayo/video/upload/v1630710623/cusmik/Snowflake_yjnwea.mp3"
-	},
-	{
-		id: "9",
-		title: "When To Say When",
-		artistes: ["Drake"],
-		imgSource: "./imgs/drake.jpg",
-		musicSrc:
-			"https://res.cloudinary.com/temitayo/video/upload/v1630710497/cusmik/When_To_Say_When_ysa87p.mp3"
-	},
-	{
-		id: "10",
-		title: "Wrist",
-		artistes: ["Chris Brown"],
-		imgSource: "./imgs/drake.jpg",
-		musicSrc: "https://res.cloudinary.com/temitayo/video/upload/v1630710517/cusmik/Wrist_otpuzz.mp3"
+			"https://ncs.io/track/download/e7884c05-656b-4911-9a36-34e060706645"
 	}
 ];
